fbf30bc8d0b30ae07d12435133be39eb10d760e8
aliciapflaumer <alpfla7@gmail.com>
2017-10-11 18:21:50 -0400

    Added CSS and reset field jQuery method
    - Added background color, text color to submit buttons
    - Added style to user facing message
    - added reset form jQuery method to ui.js text fields

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,15 +2,23 @@
 
 const store = require('../store')
 
+const resetForm = function resetForm ($form) {
+  $form.find('input:text, input:password, input:file, select, textarea').val('')
+  $form.find('input:radio, input:checkbox')
+    .removeAttr('checked').removeAttr('selected')
+}
+
 const signUpSuccess = function (data) {
   console.log(data)
   console.log('Successfully signed up')
   $('#message').text('Successfully signed up')
+  resetForm($('#sign-up'))
 }
 
 const signUpFailure = function (error) {
   console.error(error)
   $('#message').text('Error on sign up')
+  resetForm($('#sign-up'))
 }
 
 const signInSuccess = function (data) {
@@ -18,26 +26,30 @@ const signInSuccess = function (data) {
   console.log('Successfully signed in')
   $('#message').text('Successfully signed in')
   store.user = data.user
+  resetForm($('#sign-in'))
 }
 
 const signInFailure = function (error) {
   console.error(error)
   $('#message').text('Error on sign in')
+  resetForm($('#sign-in'))
 }
 
 const changePasswordSuccess = function () {
   console.log('Successfully changed password')
   $('#message').text('Successfully changed password!')
+  resetForm($('#change-password'))
 }
 
 const changePasswordFailure = function (error) {
   console.log(error)
   $('#message').text('Error on change password!')
+  resetForm($('#change-password'))
 }
 
 const signOutSuccess = function () {
   console.log('Successfully signed out')
-  $('#message').text('Successfully changed password!')
+  $('#message').text('Successfully signed out!')
   store.user = null // empty store
 }
 
