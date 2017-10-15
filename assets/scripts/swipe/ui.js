1221e0a819fad662cba4a8b11f559b34fa84fcb1
mdcollins80 <mdcollins80@gmail.com>
2017-10-15 18:55:26 -0400

    Adds checkout ui handling
    Adds checkout success and failure handling.  Clears out the shopping
    cart and sets store.order to null on successful checkout.
    
    Contributors:
    stgeorge3
    mdcollins80

--- assets/scripts/swipe/ui.js
+++ assets/scripts/swipe/ui.js
@@ -1,9 +1,21 @@
 'use strict'
 
 const store = require('../store')
+const ordersApi = require('../orders/api.js')
 
+const onCheckoutSuccess = function () {
+  ordersApi.updateOrderCompleted()
+    .then(() => $('#message').text('Your order has been purchased.  Thanks for shopping with Nozama.com!'))
+    .catch(() => $('#message').text('Your charge is complete, but your order has not been updated to purchased.  We will work to rectify this minor error. Do NOT submit your order again or you will be charged twice.'))
+  $('.shopping-cart').empty()
+  store.order = null // removes the stored order
+}
 
+const onCheckoutFailure = function () {
+  $('#message').text('We apologize for the inconvenience, but there was an error with your purchase.  Please try again.')
+}
 
 module.exports = {
-
+  onCheckoutSuccess,
+  onCheckoutFailure
 }
