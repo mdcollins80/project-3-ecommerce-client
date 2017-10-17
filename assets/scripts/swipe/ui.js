501ddb53e64a052df7947eb8e3868f160975f531
aliciapflaumer <alpfla7@gmail.com>
2017-10-17 16:10:58 -0400

    Added fadeout to swipe ui messages
    - For onCheckoutSuccess and on CheckoutFailure,
    added fadeOut to messages.

--- assets/scripts/swipe/ui.js
+++ assets/scripts/swipe/ui.js
@@ -6,8 +6,8 @@ const ordersUi = require('../orders/ui')
 const onCheckoutSuccess = function () {
   // console.log('checkout success reached')
   ordersApi.updateOrderCompleted()
-    .then(() => $('#message').show().text('Your order has been purchased.  Thanks for shopping with Nozama.com!'))
-    .catch(() => $('#message').show().text('Your charge is complete, but your order has not been updated to purchased.  We will work to rectify this minor error. Do NOT submit your order again or you will be charged twice.'))
+    .then(() => $('#message').show().text('Your order has been purchased.  Thanks for shopping with Nozama.com!').show().fadeOut(8000))
+    .catch(() => $('#message').show().text('Your charge is complete, but your order has not been updated to purchased.  We will work to rectify this minor error. Do NOT submit your order again or you will be charged twice.').show().fadeOut(8000))
   $('.shopping-cart').empty()
   $('#buttonCheckout').hide()
   ordersApi.createOrder()
@@ -16,7 +16,7 @@ const onCheckoutSuccess = function () {
 }
 
 const onCheckoutFailure = function () {
-  $('#message').text('We apologize for the inconvenience, but there was an error with your purchase.  Please try again.')
+  $('#message').text('We apologize for the inconvenience, but there was an error with your purchase.  Please try again.').show().fadeOut(8000)
 }
 
 module.exports = {
